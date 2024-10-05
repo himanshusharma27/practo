8f09c16a74e1532948a7a732f717518bd9f5308f
Himanshu <himanshuitm27@gmail.com>
2024-10-05 13:57:29 +0530

    i added the file

---
+++ main.sql
@@ -0,0 +1,13 @@
+create database college ;
+use college;
+create table student(
+id int primary key auto_increment,
+name varchar(50),
+age int,
+city varchar(50)
+);
+insert into student(name,age,city) values('sai',22,'hyd');
+insert into student(name,age,city) values('sai',22,'hyd');
+insert into student(name,age,city) values('sai',22,'hyd');
+insert into student(name,age,city) values('sai',22,'hyd');
+insert into student(name,age,city) values('sai',22,'hyd');
